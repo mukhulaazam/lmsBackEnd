4e53723a9adc6acbaeb2802e338a7674c24edb9a
Dr. Mamun <mukhulaazam@gmail.com>
2023-04-23 23:57:06 +0600

    TDD start with Keyword and tag next is complete task

---
+++ lv-lms-course-builder/app/Http/Controllers/KeywordTagController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KeywordTag;
+use App\Http\Requests\StoreKeywordTagRequest;
+use App\Http\Requests\UpdateKeywordTagRequest;
+
+class KeywordTagController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKeywordTagRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KeywordTag $keywordTag)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KeywordTag $keywordTag)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKeywordTagRequest $request, KeywordTag $keywordTag)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KeywordTag $keywordTag)
+    {
+        //
+    }
+}
